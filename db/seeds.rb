10eba3de9ab716fc351646378b475014c51c4c81
Sasha Scherrer <sasha.burkhardt@hotmail.fr>
2023-06-07 10:04:14 +0200

    seeds for users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# users.rb
+User.create!(
+  email: 'john@example.com',
+  encrypted_password: 'password',
+  first_name: 'John',
+  last_name: 'Doe'
+)
+
+User.create!(
+  email: 'jane@example.com',
+  encrypted_password: 'password',
+  first_name: 'Jane',
+  last_name: 'Smith'
+)
+
+
+
+
+
+
+# # Sasha's attempt at the responder + incident seeds
+# Incident.create!(
+#   user_id: 1,
+#   location: 'Location 1',
+#   is_closed: false
+# )
+
+# Incident.create!(
+#   user_id: 2,
+#   location: 'Location 2',
+#   is_closed: true
+# )
+
+# # responders.rb
+# Responder.create!(
+#   incident_id: 1,
+#   user_id: 1,
+#   location: 'Location 1',
+#   has_accepted: true
+# )
+
+# Responder.create!(
+#   incident_id: 2,
+#   user_id: 2,
+#   location: 'Location 2',
+#   has_accepted: false
+# )
